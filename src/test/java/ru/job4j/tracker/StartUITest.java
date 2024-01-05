d4d73b5d0a683496935d229d1debab7eb033611a
Konstantin Podgorny <podgorny.kv@gmail.com>
2024-01-05 19:29:33 +0400

    Именование классов реализующих интерфейс [#505124]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.action.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -15,8 +16,8 @@ class StartUITest {
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(output),
-                new ExitAction(output)
+                new Create(output),
+                new Exit(output)
         };
         new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
@@ -32,8 +33,8 @@ class StartUITest {
                 new String[]{"0", "1", replacedName, "1"}
         );
         UserAction[] actions = {
-                new ReplaceAction(output),
-                new ExitAction(output)
+                new Replace(output),
+                new Exit(output)
         };
         new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
@@ -48,8 +49,8 @@ class StartUITest {
                 new String[]{"0", "1", "1"}
         );
         UserAction[] actions = {
-                new DeleteAction(output),
-                new ExitAction(output)
+                new Delete(output),
+                new Exit(output)
         };
         new StartUI(output).init(input, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
@@ -63,7 +64,7 @@ class StartUITest {
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new ExitAction(out)
+                new Exit(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
@@ -83,8 +84,8 @@ class StartUITest {
                 new String[]{"0", String.valueOf(one.getId()), replaceName, "1"}
         );
         UserAction[] actions = new UserAction[]{
-                new ReplaceAction(output),
-                new ExitAction(output)
+                new Replace(output),
+                new Exit(output)
         };
         new StartUI(output).init(input, tracker, actions);
         String ln = System.lineSeparator();
@@ -110,8 +111,8 @@ class StartUITest {
                 new String[]{"0", "Any item", "1"}
         );
         UserAction[] actions = {
-                new FindByNameAction(out),
-                new ExitAction(out)
+                new FindByName(out),
+                new Exit(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
@@ -135,8 +136,8 @@ class StartUITest {
                 new String[]{"0", "1", "1"}
         );
         UserAction[] actions = {
-                new FindByIdAction(out),
-                new ExitAction(out)
+                new FindById(out),
+                new Exit(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
@@ -160,8 +161,8 @@ class StartUITest {
                 new String[]{"0", "1"}
         );
         UserAction[] actions = {
-                new FindAllAction(out),
-                new ExitAction(out)
+                new FindAll(out),
+                new Exit(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
